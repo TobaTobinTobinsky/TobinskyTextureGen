3e6d79a2f39a294e34b662f25d58e0385bf46608
Mauricio José Tobares <carrozadelamuerte@gmail.com>
2024-12-17 11:39:22 -0300

    documentación y cambios menores

--- edge.py
+++ edge.py
@@ -1,19 +1,35 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
+# ----------------------------------------------------------------------------
+#  File:        edge.py
+#  Module:      Edge
+#  Description: Módulo para generar el mapa de bordes.
 #
-# Author:      Mauricio
-#
-# Created:     15/12/2024
-# Copyright:   (c) Mauricio 2024
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+#  Author:      Mauricio José Tobares
+#  Created:     15/12/2024
+#  Copyright:   (c) 2024 Mauricio José Tobares
+#  License:     MIT License
+# ----------------------------------------------------------------------------
 
 from PIL import Image, ImageFilter, ImageEnhance
 
-def generate_edge_map(diffuse_image, smoothness_map, edge_intensity):
-    """Genera un mapa de bordes usando un filtro de contorno e influenciado por el mapa de suavidad."""
+def generate_edge_map(diffuse_image: Image.Image, smoothness_map: Image.Image, edge_intensity: float) -> Image.Image:
+    """
+    Genera un mapa de bordes usando un filtro de contorno e influenciado por el mapa de suavidad.
+
+    Este mapa de bordes se crea aplicando un filtro de detección de bordes a la imagen
+    diffuse en escala de grises. Luego, los bordes se atenúan en función del mapa de
+    suavidad y su intensidad final se ajusta.
+
+    Args:
+        diffuse_image (PIL.Image.Image): La imagen diffuse de entrada.
+        smoothness_map (PIL.Image.Image): El mapa de suavidad que influencia el resultado.
+        edge_intensity (float): La intensidad del brillo a aplicar al mapa de bordes (0-100).
+
+    Returns:
+        PIL.Image.Image: El mapa de bordes generado.
+    """
+    # Convierte la imagen diffuse a escala de grises
     gray_image = diffuse_image.convert('L')
+    # Aplica un filtro para detectar bordes
     edge_map = gray_image.filter(ImageFilter.FIND_EDGES)
 
     # Ajustar intensidad de los bordes en función del suavizado
@@ -38,6 +54,6 @@ def generate_edge_map(diffuse_image, smoothness_map, edge_intensity):
 
     # Ajustar intensidad usando brillo
     enhancer = ImageEnhance.Brightness(edge_map)
-    edge_map = enhancer.enhance(edge_intensity)
+    edge_map = enhancer.enhance(edge_intensity * 0.01)  # Se multiplica el porcentaje por un valor para conseguir más brillo
 
     return edge_map
