c913efeec53c12773c6618edacd65d4c177f871d
Mauricio José Tobares <carrozadelamuerte@gmail.com>
2024-12-17 11:39:57 -0300

    documentación y cambios menores

--- ao.py
+++ ao.py
@@ -1,23 +1,37 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
+# ----------------------------------------------------------------------------
+#  File:        ao.py
+#  Module:      AO
+#  Description: Módulo para generar el mapa de oclusión ambiental (AO).
 #
-# Author:      Mauricio
-#
-# Created:     15/12/2024
-# Copyright:   (c) Mauricio 2024
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+#  Author:      Mauricio José Tobares
+#  Created:     15/12/2024
+#  Copyright:   (c) 2024 Mauricio José Tobares
+#  License:     MIT License
+# ----------------------------------------------------------------------------
 
 from PIL import Image, ImageEnhance
 
-def generate_ao_map(diffuse_image, ao_intensity):
-    """Genera un mapa de oclusión ambiental donde todo es blanco (puede ajustarse) en formato RGB."""
+def generate_ao_map(diffuse_image: Image.Image, ao_intensity: float) -> Image.Image:
+    """
+    Genera un mapa de oclusión ambiental donde todo es blanco (puede ajustarse) en formato RGB.
+
+    Este mapa se utiliza para simular sombras de oclusión ambiental, que indican qué tan expuesta
+    está una superficie a la iluminación ambiente. Por defecto, genera una imagen completamente
+    blanca que puede ser ajustada en intensidad.
+
+    Args:
+        diffuse_image (PIL.Image.Image): La imagen diffuse de entrada (utilizada solo para obtener las dimensiones).
+        ao_intensity (float): La intensidad del brillo a aplicar al mapa de oclusión ambiental (0-100).
+
+    Returns:
+        PIL.Image.Image: El mapa de oclusión ambiental generado.
+    """
     width, height = diffuse_image.size
-    ao_map = Image.new('RGB', (width, height), color=(255, 255, 255)) # Todo blanco, ajustable
+    # Crear una imagen completamente blanca
+    ao_map = Image.new('RGB', (width, height), color=(255, 255, 255))
 
     # Ajustar intensidad usando brillo
     enhancer = ImageEnhance.Brightness(ao_map)
-    ao_map = enhancer.enhance(ao_intensity)
+    ao_map = enhancer.enhance(ao_intensity * 0.01)  # Se multiplica el porcentaje por un valor para conseguir más brillo
 
     return ao_map
