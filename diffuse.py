e4333e6a1740908213181c244569f3f9d2d14024
Mauricio José Tobares <carrozadelamuerte@gmail.com>
2024-12-17 11:37:01 -0300

    se agregó documentación

--- diffuse.py
+++ diffuse.py
@@ -1,15 +1,29 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
+# ----------------------------------------------------------------------------
+#  File:        diffuse.py
+#  Module:      Diffuse
+#  Description: Módulo para procesar la imagen diffuse.
 #
-# Author:      Mauricio
-#
-# Created:     15/12/2024
-# Copyright:   (c) Mauricio 2024
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+#  Author:      Mauricio José Tobares
+#  Created:     15/12/2024
+#  Copyright:   (c) 2024 Mauricio José Tobares
+#  License:     MIT License
+# ----------------------------------------------------------------------------
 
-# En este caso, no es necesario modificar el diffuse, pero puedes incluirlo por si necesitas alguna manipulación adicional
-def process_diffuse(diffuse_image):
-    """Procesa la imagen diffuse (en este caso, no se hace ninguna modificación)."""
+from PIL import Image
+
+def process_diffuse(diffuse_image: Image.Image) -> Image.Image:
+    """
+    Procesa la imagen diffuse (en este caso, no se hace ninguna modificación).
+
+    Esta función sirve como un punto de entrada para el procesamiento de la imagen diffuse,
+    aunque en su implementación actual no realiza ninguna modificación.
+    Se mantiene para posibles futuras expansiones o ajustes en el procesamiento de la imagen.
+
+    Args:
+        diffuse_image (PIL.Image.Image): La imagen diffuse de entrada.
+
+    Returns:
+        PIL.Image.Image: La misma imagen diffuse de entrada (sin modificaciones).
+    """
+    # En este caso, no se realiza ninguna modificación a la imagen
     return diffuse_image
